9a0ab12fa635c854c54417707fac9318e181c8df
vikram-tech at core <vikram@techatcore.com>
2020-11-09 14:16:53 +0530

    event handling started

--- src/App.js
+++ src/App.js
@@ -11,11 +11,22 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState( {
+      persons: [
+        { name: 'vikram vishal', age: 22},
+        { name: 'vijay kumar', age: 23},
+        { name: 'satyam singh', age: 25},
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>THE BASIC REACT APP</h1>
         <p>IT IS RELLY WORKING</p>
+        <button onClick={this.switchNameHandler}>Full Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>my hobbies : playing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
