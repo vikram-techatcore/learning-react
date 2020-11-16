083eea03d064d669e63db856d1e019706b39ea48
vikram-tech at core <vikram@techatcore.com>
2020-11-16 13:04:25 +0530

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './App.module.css';
 import classes from './App.module.css';
 import Person from './Person/Person';
 //import styled from 'styled-components';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 /*const StyledButton = styled.button`
 background-color:  ${props =>props.alt ? 'red' : 'blue'};
@@ -83,12 +83,13 @@ let btnClass = [classes.button];
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}> <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              key={person.id}
+              
               changed={(event) => this.nameChangedHandler(event, person.id)} />
+              </ErrorBoundary>
           })}
         </div>
       );
