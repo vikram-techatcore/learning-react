adc6094ab062b76d63794fe0d2b57bae46f31ee9
vikram-techatcore <vikram@techatcore.com>
2020-11-17 14:59:38 +0530

    life_hooks

--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -2,7 +2,33 @@ import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-class persons extends Component {
+class Persons extends Component {
+
+// static getDerivedStateFromProps(props, state){
+//   console.log('[Person.js] getDerivedStateFromProps ');
+//   return state;
+// }
+
+componentWillReceiveProps(props){
+  console.log('[persons.js] componentWillReceiveProps', props );
+}
+
+shouldComponentUpdate(nextProps, nextState){
+  console.log('[persons.js] shouldComponentUpdate');
+  return true;
+}
+
+getSnapshotBeforeUpdate(prevProps, prevState){
+  console.log('[Person.js] getSnapshotBeforeUpdate');
+  return {message: 'snapshot'};
+}
+
+componentDidUpdate(prevProps, prevState,snapshot){
+  console.log('[Person.js] componentDidUpdate');
+  console.log(snapshot);
+}
+
+
   render(){
     console.log('[Persons.js] rendering...');
     return this.props.persons.map((person, index) => {
@@ -20,4 +46,4 @@ class persons extends Component {
  
 };
 
-export default persons;
+export default Persons;
