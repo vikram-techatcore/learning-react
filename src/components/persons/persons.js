10b542472b8f1f2d5f04b85c16821e26dcb30dbf
vikram-tech at core <vikram@techatcore.com>
2020-11-17 14:17:45 +0530

    life_cycle

--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-const persons = props => {
-  console.log('[Persons.js] rendering...');
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={event => props.changed(event, person.id)}
-      />
-    );
-  });
+class persons extends Component {
+  render(){
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  };
+ 
 };
 
 export default persons;
